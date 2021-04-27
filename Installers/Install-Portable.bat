80416947a6d23538ffbb010a33c62176bd76a9bc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-04-27 20:15:22 +0200

    remove portable installer
    github does not allow downloading single files

--- Installers/Install-Portable.bat
+++
@@ -1,3 +0,0 @@
-PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Dot-lua/Dotter/0.0.3/Installers/Install.ps1'))"
-
-echo "Download finished! You can now delete the Installer!"
