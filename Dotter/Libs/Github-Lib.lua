dbb5fa58a7aea10fdad56e677e37c0caeb69120b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-04-30 17:14:44 +0200

    gitlib rework
    cache the output

--- Dotter/Libs/Github-Lib.lua
+++ Dotter/Libs/Github-Lib.lua
@@ -7,6 +7,22 @@ local Repro = "https://api.github.com/repos/Dot-lua/Dotter/releases"
 local FS = require('fs')
 local Read, Write, ReadDir = FS.readFileSync, FS.writeFileSync, FS.readdirSync
 
+_G.RemoteVersion = RemoteVersion or GetRemote()
+
+function GetRemote()
+    local WebRequest, WebBody
+    local Running = true
+    local Times = 0
+    
+    WebRequest, WebBody = Http.request("GET", Repro, {{"User-Agent", "Dotter Compiler"}})
+
+    local Decode = Json.decode(WebBody)
+
+    _G.RemoteVersion = Decode[1].tag_name
+
+    return Decode[1].tag_name
+end
+
 function Lib.GetVersion()
     local FileData = Read("./Dotter/Config/VersionData.json")
     local Decode = Json.decode(FileData)
@@ -15,15 +31,11 @@ function Lib.GetVersion()
 end
 
 function Lib.GetRemoteVersion()
-    
-    local WebRequest, WebBody
-    local Running = true
-    local Times = 0
-    
-    WebRequest, WebBody = Http.request("GET", Repro, {{"User-Agent", "Dotter Compiler"}})
-
-    local Decode = Json.decode(WebBody)
-    return Decode[1].tag_name
+    if RemoteVersion then
+        return RemoteVersion
+    else
+        return GetRemote()
+    end
 end
 
 function Lib.IsLatest()
