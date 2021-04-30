9be7a7e4a722de56fe422398e94743f70ac9f08f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-04-30 17:14:49 +0200

    Update Main.lua

--- Dotter/Main.lua
+++ Dotter/Main.lua
@@ -4,6 +4,13 @@ coroutine.wrap(function ()
     local Args = args
     table.remove(Args, 0)
     table.remove(Args, 1)
+    local GitLib = require("./Libs/Github-Lib.lua")
+
+    if not GitLib.IsLatest() then
+        print("WARNING - You are running an older version of Dotter!")
+        print("Reinstall Dotter! - View how to do so at http://Dotter.cubicinc.ga/reinstall")
+        print("Your version: " .. GitLib.GetVersion() .. " Remote version: " .. GitLib.GetRemoteVersion())
+    end
     
     _G.Tasks = {
         Run = require("./Tasks/Run.lua"),
@@ -15,7 +22,7 @@ coroutine.wrap(function ()
         Tasks.Help(Args)
     end
     
-    print(require("./Libs/Github-Lib.lua").IsLatest())
+    
     
     
 end)()
