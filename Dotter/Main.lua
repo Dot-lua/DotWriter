21f21d88ffefa92787a9455fbd3dfb8f44c9bdcc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-04-30 17:00:25 +0200

    coroutine it al

--- Dotter/Main.lua
+++ Dotter/Main.lua
@@ -1,15 +1,22 @@
-local Args = args
-table.remove(Args, 0)
-table.remove(Args, 1)
+coroutine.wrap(function ()
 
-_G.Tasks = {
-    Run = require("./Tasks/Run.lua"),
-    Build = require("./Tasks/Build.lua"),
-    Help = require("./Tasks/Help.lua")
-}
 
-if not Args[1] then
-    Tasks.Help(Args)
-end
-
-print(require("./Libs/Github-Lib.lua").GetRemoteVersion())
+    local Args = args
+    table.remove(Args, 0)
+    table.remove(Args, 1)
+    
+    _G.Tasks = {
+        Run = require("./Tasks/Run.lua"),
+        Build = require("./Tasks/Build.lua"),
+        Help = require("./Tasks/Help.lua")
+    }
+    
+    if not Args[1] then
+        Tasks.Help(Args)
+    end
+    
+    print(require("./Libs/Github-Lib.lua").IsLatest())
+    
+    
+end)()
+    
