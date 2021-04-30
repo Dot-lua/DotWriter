a4e200f1c245f136387e23fb450e60b28f453b20
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-04-30 14:58:29 +0200

    Update Github-Lib.lua

--- Dotter/Libs/Github-Lib.lua
+++ Dotter/Libs/Github-Lib.lua
@@ -1,7 +1,14 @@
 local Lib = {}
+local Json = require("json")
+
+local FS = require('fs')
+local Read, Write = fs.readFileSync, fs.writeFileSync
 
 function Lib.GetVersion()
-    
+    local FileData = Read("../Config/VersionData.json")
+    local Decode = Json.decode(FileData)
+
+    return Decode["Tag-Version"]
 end
 
 return Github
