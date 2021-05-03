bd820a4d875f0c3d5be53e7063cfec321ff3508e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-03 14:04:39 +0200

    handle commands

--- Dotter/Main.lua
+++ Dotter/Main.lua
@@ -5,25 +5,42 @@ coroutine.wrap(function ()
     local Args = args
     table.remove(Args, 0)
     table.remove(Args, 1)
-    local GitLib = require("./Libs/Github-Lib.lua")
+
+    _G.WorkingOS = Args[1]
+    table.remove(Args, 1)
+
+    _G.GitLib = require("Github-Lib")
     _G.Logger = require("Logger")
+    _G.BuildHelper = require("BuildHelper")
 
     if not GitLib.IsLatest() then
-        Logger.Warn("WARNING - You are running an older version of Dotter!")
+        Logger.Warn("You are running an older version of Dotter!")
         Logger.Warn("Reinstall Dotter! - View how to do so at http://Dotter.cubicinc.ga/reinstall")
         Logger.Warn("Your version: " .. GitLib.GetVersion() .. " Remote version: " .. GitLib.GetRemoteVersion())
         print()
     end
     
     _G.Tasks = {
-        Run = require("./Tasks/Run.lua"),
-        Build = require("./Tasks/Build.lua"),
-        Help = require("./Tasks/Help.lua")
+        run = require("./Tasks/Run.lua"),
+        build = require("./Tasks/Build.lua"),
+        help = require("./Tasks/Help.lua"),
+        init = require("./Tasks/Init.lua")
     }
     
     if not Args[1] then
-        Tasks.Help(Args)
+        Tasks.help(Args)
+    else
+        local Command = string.lower(Args[1])
+
+        if Tasks[Command] then
+            Tasks[Command]()
+        else
+            BuildHelper.Fail("Command '" .. Command .. "' Does not exist!")
+        end
+
     end
+
+
     
     print()
     
