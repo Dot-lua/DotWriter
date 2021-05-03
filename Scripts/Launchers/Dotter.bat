c990dccbde8466a016b420fb43cf287d16050cca
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-03 14:05:08 +0200

    pass os

--- Scripts/Launchers/Dotter.bat
+++ Scripts/Launchers/Dotter.bat
@@ -1,1 +1,1 @@
-Powershell -Command "./Dotter/Envoirment/Luvit/luvit ./Dotter/Main.lua %1 %2 %3 %4 %5 %6 %7 %8 %9"
+Powershell -Command "./Dotter/Envoirment/Luvit/luvit ./Dotter/Main.lua Windows %1 %2 %3 %4 %5 %6 %7 %8 %9"
