9657715c2d6bca33d3b28bee88498a6e23d601e2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-03 14:04:21 +0200

    Create BuildHelper.lua

---
+++ Dotter/Libs/BuildHelper.lua
@@ -0,0 +1,62 @@
+
+--[[
+	Hello there, reader of this file.
+	i just want to note to you:
+	i reworked the discordia logger from
+	https://github.com/SinisterRectus/Discordia/blob/master/libs/utils/Logger.lua
+	-- Thanks
+]]
+
+local date = os.date
+local format = string.format
+local stdout = _G.process.stdout.handle
+
+local BLACK   = 30
+local RED     = 31
+local GREEN   = 32
+local YELLOW  = 33
+local BLUE    = 34
+local MAGENTA = 35
+local CYAN    = 36
+local WHITE   = 37
+
+local config = {
+	{'BUILD FAILED', RED},
+	{'BUILD COMPLETE', GREEN},
+}
+
+local BuildHelper = {}
+
+do -- parse config
+	local bold = 1
+	for _, v in ipairs(config) do
+		v[2] = format('\27[%i;%im%s\27[0m', bold, v[2], v[1])
+	end
+end
+
+
+function BuildHelper.Log(level, msg, ...)
+
+	local tag = config[level]
+	if not tag then return end
+
+    msg = msg or ""
+
+	msg = format(msg, ...)
+
+	local d = date("%Y-%m-%d %H:%M:%S")
+	stdout:write(format('%s %s\n', tag[2], msg))
+
+	return msg
+
+end
+
+function BuildHelper.Fail(Msg, ...)
+	BuildHelper.Log(1, Msg, ... or "")
+end
+
+function BuildHelper.Complete(Msg, ...)
+	BuildHelper.Log(2, Msg, ... or "")
+end
+
+return BuildHelper
