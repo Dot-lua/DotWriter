6e26f647e2f91e6e8fed3a376d16a913db59a20a
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-05-28 08:21:48 +0200

    Create Cleaner.sh

---
+++ Dotter/Scripts/Cleaners/Cleaner.sh
@@ -0,0 +1,7 @@
+echo "Cleaning output folder.."
+rm ./Dotter/Output/
+mkdir ./Dotter/Output/
+mkdir ./Dotter/Output/Cache
+mkdir ./Dotter/Output/Libraries
+mkdir ./Dotter/Output/Data
+mkdir ./Dotter/Output/Build
