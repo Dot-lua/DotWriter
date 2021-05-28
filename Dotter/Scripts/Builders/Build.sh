2d21de22e638dc5a815d72fbf97a8aab69e1ab82
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-05-28 08:21:44 +0200

    work on buildscript

--- Dotter/Scripts/Builders/Build.sh
+++ Dotter/Scripts/Builders/Build.sh
@@ -1,41 +1,32 @@
-$output = "${1}"
+OUTPUT=${1?ERROR}
 
 
-echo "Generating output to $output.dua"
+echo "Generating output to $OUTPUT.dua"
 sleep 1
 
-echo "Cleaning output folder.."
-rm ./Dotter/Output/
-mkdir ./Dotter/Output/
-mkdir ./Dotter/Output/Cache
-mkdir ./Dotter/Output/Libraries
-mkdir ./Dotter/Output/Data
-mkdir ./Dotter/Output/Build
-
-pwd > echo
-
 echo "Cloning src folder..."
-cp ./src ./Dotter/Output/Data
+cp -R ./src ./Dotter/Output/Data/
 
 echo "Unpacking main folder to src..."
-cp ./Dotter/Output/Data/src/main/* ./Dotter/Output/Data/src/
+mv ./Dotter/Output/Data/src/main/* ./Dotter/Output/Data/src/
 
 echo "Unpacking lua folder"
-cp ./Dotter/Output/Data/src/lua/* ./Dotter/Output/Data/src/
+mv ./Dotter/Output/Data/src/lua/* ./Dotter/Output/Data/src/
 
 echo "Removing lua and main folder"
-rm ./Dotter/Output/Data/src/lua/
-rm ./Dotter/Output/Data/src/main/
+rm -R ./Dotter/Output/Data/src/lua/
+rm -R ./Dotter/Output/Data/src/main/
+rm -R ./Dotter/Output/Data/src/src/
 
 echo "Compressing dua"
-zip ./Dotter/Output/Data/src/ ./Dotter/Output/Cache/Out.zip
+zip -R -X ./Dotter/Output/Cache/Out ./Dotter/Output/Data/src/*
 sleep 1
 
 echo "Cleaning archive"
-mv ./Dotter/Output/Cache/Out.zip ./Dotter/Output/Cache/$output.dua
+mv ./Dotter/Output/Cache/Out.zip ./Dotter/Output/Cache/$OUTPUT.dua
 
 echo "Moving archive"
-cp ./Dotter/Output/Cache/$output.dua  -Destination ./Dotter/Output/build/$output.dua
+cp -R ./Dotter/Output/Cache/$OUTPUT.dua ./Dotter/Output/build/$OUTPUT.dua
 
 echo "Done!"
 sleep 1
