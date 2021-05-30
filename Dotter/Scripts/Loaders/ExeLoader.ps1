41725a9407493567252822802cac0c4723e9d94b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-30 12:12:22 +0200

    Update ExeLoader.ps1

--- Dotter/Scripts/Loaders/ExeLoader.ps1
+++ Dotter/Scripts/Loaders/ExeLoader.ps1
@@ -1,1 +1,32 @@
-Pause
+$Stage = $args[0]
+
+If ($Stage -Match "StageTwo")
+{
+    Write-Output "DOT-LUA"
+    Write-Output ""
+    Write-Output "Copyright (C) 2021 CoreByte"
+    Write-Output "Copyright (C) 2021 Dot-lua"
+    Write-Output "Copyright (C) 2021 Cubic inc"
+
+    Write-Output "Loading file..."
+}
+Else
+{
+    $shell = New-Object -ComObject WScript.Shell
+    $shortcut = $shell.CreateShortcut("./Loading.lnk")
+    $shortcut.IconLocation = "$PSScriptRoot/Icon.ico"
+    $shortcut.TargetPath = "Powershell.exe"
+    $shortcut.Arguments = "-NoProfile -ExecutionPolicy unrestricted -File $PSScriptRoot/ExeLoader.ps1 StageTwo"
+    $shortcut.Save()
+
+    #Start-Process ./Loading.lnk
+    ./Loading.lnk
+    Write-Output "Started!"
+    Write-Output "This window will now terminate!"
+    
+}
+Start-Sleep 1
+If ($Stage -Match "StageTwo")
+{
+    Pause
+}
