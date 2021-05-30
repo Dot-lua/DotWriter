6ce542b9113762fae75a7dca51512ddfe27ea5ca
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-05-30 12:12:36 +0200

    switch around some things

--- Dotter/Scripts/Executeable/Executeable.ps1
+++ Dotter/Scripts/Executeable/Executeable.ps1
@@ -4,23 +4,27 @@ PowerShell -ExecutionPolicy unrestricted -File ./Dotter/Scripts/Builders/Build.p
 
 Write-Output "Getting ready to create executeable"
 
-Write-Output "Creating exe"
-iexpress.exe /N ./Dotter/Output/Cache/ExeData.sed
-
-
 Write-Output "Compiling rc"
-.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log CON -open .\Dotter\Config\ExecuteableInfo.rc -save .\Dotter\Output\Cache\Information.res -action compile 
+.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log NUL -open .\Dotter\Config\ExecuteableInfo.rc -save .\Dotter\Output\Cache\Information.res -action compile 
 
 Write-Output "Resizing icon"
 .\Dotter\Envoirment\ffmpeg\ffmpeg -hide_banner -loglevel error -i .\Dotter\Config\Icon.png -vf scale=256:256 .\Dotter\Output\Cache\Icon.ico
 
+Start-Sleep 1
+
+Write-Output "Creating exe"
+iexpress.exe /N ./Dotter/Output/Cache/ExeData.sed
+
+Start-Sleep 1
+
 Write-Output "Adding ico to EXE"
-.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log CON -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -action addoverwrite -res .\Dotter\Output\Cache\Icon.ico -mask ICONGROUP,MAINICON
+.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log NUL -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -action addoverwrite -res .\Dotter\Output\Cache\Icon.ico -mask ICONGROUP,MAINICON
 
 Write-Output "Adding res to EXE"
-.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log CON -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -res .\Dotter\Output\Cache\Information.res -action addoverwrite -mask "VERSIONINFO,1,1033"
-.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log CON -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -res .\Dotter\Output\Cache\Information.res -action addoverwrite -mask "VERSIONINFO,1,1043"
+.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log NUL -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -res .\Dotter\Output\Cache\Information.res -action addoverwrite -mask "VERSIONINFO,1,1033"
+.\Dotter\Envoirment\ResourceHacker\ResourceHacker.exe -log NUL -open .\Dotter\Output\Cache\Out.exe -save .\Dotter\Output\Cache\Out.exe -res .\Dotter\Output\Cache\Information.res -action addoverwrite -mask "VERSIONINFO,1,1043"
 
+Start-Sleep 1
 
 Write-Output "Refreshing icons"
 cmd /c ie4uinit.exe -show
